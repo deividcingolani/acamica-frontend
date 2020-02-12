6796341413d358324ee63bfeac02cf508135cc83
david <cingolanidavid@gmail.com>
2020-02-12 12:29:23 -0300

    prod

--- src/lib/auth0-variables.js
+++ src/lib/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'dev-acamica2.auth0.com',
   clientId: 'v1DwgMtY6lgeBJ9qglyAT1wANZJjX0gO',
-  callbackUrl: 'https://acamica-frontend.now.sh/'
+  callbackUrl: 'https://acamica-frontend.now.sh/auth/callback'
 }
